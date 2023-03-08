dcf022b92f6ce63089a948dcfd02d68aa8fd2634
zachmeyner <zacharymeyner@gmail.com>
2023-03-08 15:30:14 -0500

    i dunno what's broken

--- rust/extended_seive/src/main.rs
+++ rust/extended_seive/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    sieve(/*prime,*/ 100);
+    extended_sieve(100);
     println!();
 }
 
-fn sieve(/*prime: &mut Vec<usize>,*/ limit: usize) {
+fn sieve(prime: &mut Vec<usize>, limit: usize) {
     let mut mark = vec![true; limit + 1];
 
     mark.iter_mut().skip(4).step_by(2).for_each(|p| *p = false);
@@ -16,9 +16,47 @@ fn sieve(/*prime: &mut Vec<usize>,*/ limit: usize) {
         }
     }
 
-    for (n, b) in mark.into_iter().enumerate().skip(2) {
-        if b {
-            print!("{} ", n)
+    for (idx, val) in mark.into_iter().enumerate().skip(2) {
+        if val {
+            prime.push(idx);
+            print!("{} ", idx)
         }
+    }
+}
+
+fn extended_sieve(limit: usize) {
+    let size: usize = f64::sqrt(limit as f64) as usize + 1;
+    let mut prime: Vec<usize> = vec![];
+    sieve(&mut prime, size);
+    println!("{:?}", prime);
+
+    let mut low = size;
+    let mut high = 2 * size;
+
+    while low < limit {
+        if high >= limit {
+            high = limit;
+        }
+
+        let mut visited = vec![true; size + 1];
+
+        for val in 0..prime.len() {
+            let mut low_lim = (low / prime[val]) as usize * prime[val];
+
+            if low_lim < low {
+                low_lim += prime[val];
+            }
+
+            for j in (low_lim..high).step_by(prime[val]) {
+                visited[j - low] = false;
+            }
+        }
+        for i in low..high {
+            if visited[i - low] {
+                // print!("{} ", i);
+            }
+        }
+        low = low + limit;
+        high = high + limit;
     }
 }
