ac2b4798b9537406afcc8728f6054a64837f71f2
zachmeyner <zacharymeyner@gmail.com>
2023-03-15 09:49:49 -0400

    it might be working now I check later

--- rust/pi_approx/src/main.rs
+++ rust/pi_approx/src/main.rs
@@ -1,26 +1,35 @@
 use rug::Integer;
 use rug::Rational;
-use rug::{float::Special, Float};
 
 fn main() {
-    calc_precise_to(50000);
+    calc_precise_to(100);
     // println!("{}", calc_next_sum(0));
 }
 
 fn calc_precise_to(out_to: u32) {
     // let front_const = Float::with_val(10_000, 53360 * Float::with_val(10_000, 640320).sqrt());
-    let front_const = Float::with_val(1_000_000, 53360) * Float::with_val(10_000, 640320).sqrt();
+    let front_const = Integer::from(53360)
+        * Integer::from(640320 * Integer::from(Integer::u_pow_u(10, 1400))).sqrt();
     let mut bad_pi = Rational::from(0);
-    let mut pi_approx_store = Float::with_val(1_000_000, Special::Zero);
+    let mut pi_approx_store = Integer::from(0);
 
     let mut sum_num: u32 = 0;
+
+    // println!("{}", front_const);
 
     let mut accuracy = 0;
 
     loop {
         let cpy = bad_pi.clone();
         bad_pi = cpy + calc_next_sum(sum_num);
-        let good_pi: Float = front_const.clone() * Rational::from((bad_pi.denom(), bad_pi.numer()));
+
+        let pi_ratio: Rational = &front_const * Rational::from((bad_pi.denom(), bad_pi.numer()));
+
+        let good_pi: Integer = Integer::from(pi_ratio.numer() / pi_ratio.denom());
+
+        // println!("{}", good_pi);
+
+        // println!("{}", bad_pi);
 
         // let pi_one = Integer::from(good_pi.numer() * Integer::from(Integer::u_pow_u(10, 500)))
         //     / good_pi.denom();
@@ -34,7 +43,7 @@ fn calc_precise_to(out_to: u32) {
 
         // println!("{}", accuracy);
 
-        //  println!("{}", pi_approx_store);
+        // println!("{}", pi_approx_store);
 
         if accuracy >= out_to {
             break;
@@ -80,7 +89,7 @@ fn calc_next_sum(n: u32) -> Rational {
     sign * frac_one * frac_two
 }
 
-fn compare_pi(pi1: &Float, pi2: &Float, start: u32) -> u32 {
+fn compare_pi(pi1: &Integer, pi2: &Integer, start: u32) -> u32 {
     let pi_str1 = pi1.to_string();
     let pi_str2 = pi2.to_string();
 
