bd90de5dda4b383b91a38405305b93130a80a16f
zachmeyner <zacharymeyner@gmail.com>
2023-03-29 16:08:31 -0400

    adjsuted script to know where it is

--- script/timescipt.sh
+++ script/timescipt.sh
@@ -17,40 +17,43 @@ SUMC=0
 
 sum seive 20 times and average the runtimes
 start at 1,000,000 and inc by 500,000 until reach 5,000,000
- INC=100000000
- 
- while [ $INC -le 500000000 ]
- do
-     #Rust program for seive
-     for _IT in {1..10}
-     do
-         START=$(date +%s.%N)
-         (cd ../rust/extended_seive && cargo run --release $INC > /dev/null)
-         DUR=$(echo "$(date +%s.%N) - $START" | bc)
-         SUMR=$(echo "$SUMR + $DUR" | bc)
-     done
- 
- 
-     #cpp program for seive
-     for _IT in {1..10}
-     do
-         START=$(date +%s.%N)
-         (cd ../cpp/extended_sieve && ./sieve $INC > /dev/null)
-         DUR=$(echo "$(date +%s.%N) - $START" | bc)
-         SUMC=$(echo "$SUMC + $DUR" | bc)
-     done
- 
-     AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
-     AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
-     echo "$INC,$AVGR,$AVGC" >> seivetimes.csv
-     (( INC+=50000000 ))
- done
+INC=100000000
+
+while [ $INC -le 500000000 ]
+do
+    #Rust program for seive
+    echo "Rust sieve with $INC"
+    for _IT in {1..10}
+    do
+        START=$(date +%s.%N)
+        (cd ../rust/extended_seive && cargo run --release $INC > /dev/null)
+        DUR=$(echo "$(date +%s.%N) - $START" | bc)
+        SUMR=$(echo "$SUMR + $DUR" | bc)
+    done
+
+
+    #cpp program for seive
+    echo "C++ sieve with $INC"
+    for _IT in {1..10}
+    do
+        START=$(date +%s.%N)
+        (cd ../cpp/extended_sieve && ./sieve $INC > /dev/null)
+        DUR=$(echo "$(date +%s.%N) - $START" | bc)
+        SUMC=$(echo "$SUMC + $DUR" | bc)
+    done
+
+    AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
+    AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
+    echo "$INC,$AVGR,$AVGC" >> seivetimes.csv
+    (( INC+=50000000 ))
+done
 
 INC=50000
 
 while [ $INC -le 250000 ]
 do
-    #Rust program for seive
+    #Rust program for pi
+    echo "Rust Pi with $INC"
     for _IT in {1..10}
     do
         START=$(date +%s.%N)
@@ -60,7 +63,8 @@ do
     done
 
 
-    #cpp program for seive
+    #cpp program for pi
+    echo "Rust Pi with $INC"
     for _IT in {1..10}
     do
         START=$(date +%s.%N)
