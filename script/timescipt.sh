ed5e7aaa1f2b7a05b48c1af2650b7597c25d9ad4
zachmeyner <zacharymeyner@gmail.com>
2023-04-06 13:02:47 -0400

    made values proper test values again

--- script/timescipt.sh
+++ script/timescipt.sh
@@ -21,11 +21,11 @@ SUMC=0
 # start at 1,000,000 and inc by 500,000 until reach 5,000,000
 INC=100000000
 
-while [ $INC -le 100000000 ]
+while [ $INC -le 500000000 ]
 do
     #Rust program for seive
     echo "Rust sieve with $INC"
-    for _IT in {1..3}
+    for _IT in {1..10}
     do
         (cd ../rust/extended_seive && /usr/bin/time -f %e -o ../../script/out.out cargo run --release $INC > /dev/null)
         RTIM=$(< out.out)
@@ -35,15 +35,15 @@ do
 
     #cpp program for seive
     echo "C++ sieve with $INC"
-    for _IT in {1..3}
+    for _IT in {1..10}
     do
         (cd ../cpp/extended_sieve && /usr/bin/time -f %e -o ../../script/out.out ./sieve $INC > /dev/null)
         CTIM=$(< out.out)
         SUMC=$(echo "$SUMC + $CTIM" | bc)
     done
 
-    AVGR=$(echo "scale=10; $SUMR / 3.0" | bc)
-    AVGC=$(echo "scale=10; $SUMC / 3.0" | bc)
+    AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
+    AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
     echo "$INC,$AVGR,$AVGC" >> test.csv
     (( INC+=50000000 ))
 done
@@ -52,11 +52,11 @@ INC=50000
 
 
 # Pi approx 10 times and average it
-while [ $INC -le 50000 ]
+while [ $INC -le 250000 ]
 do
     #Rust program for pi
     echo "Rust Pi with $INC"
-    for _IT in {1..3}
+    for _IT in {1..10}
     do
         (cd ../rust/pi_approx && /usr/bin/time -f %e -o ../../script/out.out cargo run --release $INC > /dev/null)
         RTIM=$(< out.out)
@@ -66,15 +66,15 @@ do
 
     #cpp program for pi
     echo "C++ Pi with $INC"
-    for _IT in {1..3}
+    for _IT in {1..10}
     do
         (cd ../cpp/pi_approx && /usr/bin/time -f %e -o ../../script/out.out ./pi_approx $INC > /dev/null)
         CTIM=$(< out.out)
         SUMC=$(echo "$SUMC + $CTIM" | bc)
     done
 
-    AVGR=$(echo "scale=10; $SUMR / 3.0" | bc)
-    AVGC=$(echo "scale=10; $SUMC / 3.0" | bc)
+    AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
+    AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
     echo "$INC,$AVGR,$AVGC" >> test2.csv
     (( INC+=50000 ))
 done
