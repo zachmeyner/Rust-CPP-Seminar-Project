4a9d49fbb2d04fd7f53240162e6f59e318d0d0cf
zachmeyner <zacharymeyner@gmail.com>
2023-04-06 12:50:07 -0400

    testing da stuff

--- script/timescipt.sh
+++ script/timescipt.sh
@@ -15,36 +15,36 @@ echo "input,Rust,C++" > pitimes.csv
 SUMR=0
 SUMC=0
 
+
+
 # sum seive 10 times and average the runtimes
-start at 1,000,000 and inc by 500,000 until reach 5,000,000
+# start at 1,000,000 and inc by 500,000 until reach 5,000,000
 INC=100000000
 
-while [ $INC -le 500000000 ]
+while [ $INC -le 100000000 ]
 do
     #Rust program for seive
     echo "Rust sieve with $INC"
-    for _IT in {1..10}
+    for _IT in {1..3}
     do
-        START=$(date +%s.%N)
-        (cd ../rust/extended_seive && cargo run --release $INC > /dev/null)
-        DUR=$(echo "$(date +%s.%N) - $START" | bc)
-        SUMR=$(echo "$SUMR + $DUR" | bc)
+        (cd ../rust/extended_seive && /usr/bin/time -f %e -o out.out cargo run --release $INC > /dev/null)
+        RTIM=$(< out.out)
+        SUMR=$(echo "$SUMR + $RTIM" | bc)
     done
 
 
     #cpp program for seive
     echo "C++ sieve with $INC"
-    for _IT in {1..10}
+    for _IT in {1..3}
     do
-        START=$(date +%s.%N)
-        (cd ../cpp/extended_sieve && ./sieve $INC > /dev/null)
-        DUR=$(echo "$(date +%s.%N) - $START" | bc)
-        SUMC=$(echo "$SUMC + $DUR" | bc)
+        (cd ../cpp/extended_sieve && /usr/bin/time -f %e -o out.out ./sieve $INC > /dev/null)
+        CTIM=$(< out.out)
+        SUMC=$(echo "$SUMC + $CTIM" | bc)
     done
 
-    AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
-    AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
-    echo "$INC,$AVGR,$AVGC" >> seivetimes.csv
+    AVGR=$(echo "scale=10; $SUMR / 3.0" | bc)
+    AVGC=$(echo "scale=10; $SUMC / 3.0" | bc)
+    echo "$INC,$AVGR,$AVGC" >> test.csv
     (( INC+=50000000 ))
 done
 
@@ -52,16 +52,15 @@ INC=50000
 
 
 # Pi approx 10 times and average it
-while [ $INC -le 250000 ]
+while [ $INC -le 50000 ]
 do
     #Rust program for pi
     echo "Rust Pi with $INC"
     for _IT in {1..10}
     do
-        START=$(date +%s.%N)
-        (cd ../rust/pi_approx && cargo run --release $INC > /dev/null)
-        DUR=$(echo "$(date +%s.%N) - $START" | bc)
-        SUMR=$(echo "$SUMR + $DUR" | bc)
+        (cd ../rust/pi_approx && /usr/bin/time -f %e -o out.out cargo run --release $INC > /dev/null)
+        RTIM=$(< out.out)
+        SUMR=$(echo "$SUMR + $RTIM" | bc)
     done
 
 
@@ -69,14 +68,13 @@ do
     echo "Rust Pi with $INC"
     for _IT in {1..10}
     do
-        START=$(date +%s.%N)
-        (cd ../cpp/pi_approx && ./pi_approx $INC > /dev/null)
-        DUR=$(echo "$(date +%s.%N) - $START" | bc)
-        SUMC=$(echo "$SUMC + $DUR" | bc)
+        (cd ../cpp/pi_approx && /usr/bin/time -f %e -o out.out ./pi_approx $INC > /dev/null)
+        CTIM=$(< out.out)
+        SUMC=$(echo "$SUMC + $CTIM" | bc)
     done
 
-    AVGR=$(echo "scale=10; $SUMR / 10.0" | bc)
-    AVGC=$(echo "scale=10; $SUMC / 10.0" | bc)
-    echo "$INC,$AVGR,$AVGC" >> pitimes.csv
+    AVGR=$(echo "scale=10; $SUMR / 3.0" | bc)
+    AVGC=$(echo "scale=10; $SUMC / 3.0" | bc)
+    echo "$INC,$AVGR,$AVGC" >> test2.csv
     (( INC+=50000 ))
 done
